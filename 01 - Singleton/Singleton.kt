fdb032cadc1cc0011647540b59b831e1f87171f9
Amin Memariani <amin90memar@gmail.com>
2018-06-28 00:58:17 +0430

    Create Singleton.kt
    Create only one object at a time

---
+++ 01 - Singleton/Singleton.kt
@@ -0,0 +1,13 @@
+object SingletonClass {
+     //Variable Declaration
+         private set
+
+    init {
+        // initialize your object
+    }
+
+    @Synchronized fun doSomething() {
+        // Put implementation here.
+    }
+
+}

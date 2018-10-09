80b21b1f0da62fdf254b39ea1c9014d94c77116a
Amin Memariani <amin90memar@gmail.com>
2018-10-09 13:07:28 +0330

    Retrofit Singletone Class

--- 01 - Singleton/Singleton.kt
+++ 01 - Singleton/Singleton.kt
@@ -1,13 +1,38 @@
-object SingletonClass {
-     //Variable Declaration
-         private set
+import gateway.Service.LiveData.LiveDataCallAdapterFactory
+import gateway.Service.WebService
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
+class RetrofitClient {
+
+    private lateinit var retrofit: Retrofit
     init {
-        // initialize your object
+        retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addCallAdapterFactory( LiveDataCallAdapterFactory())
+//                .client(client)
+                .build();
     }
 
-    @Synchronized fun doSomething() {
-        // Put implementation here.
+    fun getWebService(): WebService {
+        return retrofit.create(WebService::class.java)
     }
 
+    companion object {
+        private var myInstance: RetrofitClient? = null
+        private const val BASE_URL = "http://185.156.173.111/~twannas/"
+
+        @Synchronized
+        fun getInstance(): RetrofitClient? {
+
+            if (myInstance == null) {
+                myInstance = RetrofitClient()
+            }
+            return myInstance
+        }
+    }
 }
